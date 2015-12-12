38ded9c2a10b2584506152876f98df4a6821ea22
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2015-12-12 16:20:33 +0300

    Add "Exit" and "Reload Mixer" features

--- volume_tray.py
+++ volume_tray.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/python2
 
-
-# Copyright (c) Alexander Mishurov. All rights reserved.
+# Copyright (c) 2015 Alexander Mishurov. All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following
 # conditions are met:
@@ -36,11 +34,18 @@ import gtk
 import gobject
 from pyalsa import alsacard, alsamixer
 
+# native alsa python binding
+# http://www.alsa-project.org/
+# emerge --ask dev-python/pyalsa
+# apt-get install python-pyalsa
 
-VOLUME_WIDTH = 175
+
+VOLUME_WIDTH = 225
 VOLUME_HEIGHT = 35
 SCROLL_BY = 1
-MIXER_LABEL = "PulseAudio"
+MIXER_LABEL = "Pulseaudio..."
+RELOAD_MIXER_LABEL = "Reload Mixer"
+EXIT_LABEL = "Exit"
 MIXER_CMD = "pavucontrol"
 VOL_CTRL="Master"
 POLL_TIMEOUT=100
@@ -48,11 +53,15 @@ POLL_TIMEOUT=100
 
 class SoundIcon(object):
     def __init__(self):
-        signal.signal(signal.SIGINT, gtk.main_quit)
+        signal.signal(signal.SIGINT, self._exit)
         self.init_alsa()
         self.init_menu()
         self.init_icon()
         self.update_icon()
+
+    def _exit(self, *args):
+        self.mixer.free()
+        gtk.main_quit()
 
     def init_alsa(self):
         self.mixer = alsamixer.Mixer()
@@ -65,12 +74,20 @@ class SoundIcon(object):
         # Setup mixer changes event handling
         self.poller = select.poll()
         self.mixer.register_poll(self.poller)
+        self.handle = True
         gobject.timeout_add(POLL_TIMEOUT,
                             self.poll)
         self.poller.poll()
 
+    def reload_mixer(self, widget, *args):
+        self.mixer.free()
+        self.init_alsa()
+        self.update_slider()
+        self.update_icon()
+
     def poll(self):
-        self.mixer.handle_events()
+        if self.handle:
+            self.mixer.handle_events()
         return True
 
     def init_icon(self):
@@ -94,35 +111,60 @@ class SoundIcon(object):
 
         slider_item = gtk.ImageMenuItem()
         slider_item.add(self.slider)
-        for e in ('motion-notify-event',
-                  'button-press-event',
-                  'button-release-event',):
-            slider_item.connect(e, self.redirect_mouse, e)
+        self.redirect_events(slider_item, self.slider)
 
-        for e in ('grab-broken-event',
-                  'grab-notify',):
-            slider_item.connect(e, self.redirect_grab, e)
+        reload_button = gtk.Button(RELOAD_MIXER_LABEL)
+        reload_button.set_focus_on_click(False)
+        reload_button.connect('button-release-event',
+                              self.reload_mixer)
+        reload_mixer_item = gtk.ImageMenuItem()
+        reload_mixer_item.add(reload_button)
+        self.redirect_events(reload_mixer_item, reload_button)
 
         mixer_item = gtk.MenuItem(MIXER_LABEL)
         mixer_item.connect('activate', self.activate_mixer)
 
+        exit_item = gtk.MenuItem(EXIT_LABEL)
+        exit_item.connect('activate', self._exit)
+
         self.menu = gtk.Menu()
         self.menu.append(slider_item)
+        self.menu.append(reload_mixer_item)
         self.menu.append(mixer_item)
+        self.menu.append(exit_item)
+        self.menu.connect('show', self.on_menu_show)
+        self.menu.connect('hide', self.on_menu_hide)
         self.menu.show_all()
 
-    def redirect_mouse(self, widget, event, name):
+    def on_menu_show(self, widget):
+        self.update_slider()
+        self.handle = False
+
+    def on_menu_hide(self, widget):
+        self.handle = True
+
+    def redirect_events(self, parent, child):
+        for e in ('motion-notify-event',
+                  'button-press-event',
+                  'button-release-event',):
+            parent.connect(e, self.redirect_mouse, e, child)
+
+        for e in ('grab-broken-event',
+                  'grab-notify',):
+            parent.connect(e, self.redirect_grab, e, child)
+
+    def redirect_mouse(self, widget, event, name, child):
         parent_allocation = widget.get_allocation()
-        allocation = self.slider.get_allocation()
+        allocation = child.get_allocation()
         event.x -= allocation.x
         event.y -= allocation.y
-        event.x_root -= allocation.y
+        event.x_root -= allocation.x
         event.y_root -= allocation.y
-        self.slider.emit(name, event)
+        child.emit(name, event)
         return True
 
-    def redirect_grab(self, widget, event, name):
-        self.slider.emit(name, event)
+    def redirect_grab(self, widget, event, name, child):
+        child.emit(name, event)
         return True
 
     def activate_mixer(self, widget):
@@ -132,7 +174,6 @@ class SoundIcon(object):
         )
 
     def on_icon_left_click(self, widget):
-        self.update_slider()
         self.menu.popup(None, None, None, 1,
                         gtk.get_current_event_time())
 
@@ -144,9 +185,7 @@ class SoundIcon(object):
     def on_slide(self, widget):
         level = widget.get_value()
         level = self.volume_range[1] / 100 * level
-        gobject.idle_add(
-           self.set_volume, level
-        )
+        self.set_volume(level)
 
     def set_volume(self, level):
         volume = int(level)
@@ -188,7 +227,11 @@ class SoundIcon(object):
     def update_slider(self):
         volume = self.element.get_volume()
         level = volume / (self.volume_range[1] / 100)
-        self.slider.set_value(level)
+        try:
+            self.slider.set_value(level)
+        except IOError:
+            self.reload_mixer(self.slider)
+            self.slider.set_value(level)
 
 
 if __name__ == '__main__':
